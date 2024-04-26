15281c4ed4b6ecf81a847fdc78fcab4bf629d8fc
Amanda <amanda@depaula.xyz>
2024-04-26 13:20:21 -0400

    fix to contact page button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -36,35 +36,33 @@ const contactFormOptions = [
 const ContactForm = () => {
   return contactFormOptions.map((input: InputLabels) => {
     return (
-      <>
-        <div className="flex flex-row space-x-3 justify-center" key={input.id}>
-          {input.icon}
-          {input.id === 3 ? (
-            <textarea
-              className="rounded-sm placeholder-gray-500 bg-transparent w-1/2 overflow-scroll h-full max-h-60"
-              placeholder={input.placeholder}
-            ></textarea>
-          ) : (
-            <input
-              className="rounded-sm placeholder-gray-500 bg-transparent w-1/2"
-              name={input.name}
-              placeholder={input.placeholder}
-            />
-          )}
-        </div>
-      </>
+      <div className="flex flex-row space-x-3 justify-center" key={input.id}>
+        {input.icon}
+        {input.id === 3 ? (
+          <textarea
+            className="rounded-md placeholder-gray-500  bg-transparent w-1/2 overflow-scroll h-full max-h-60"
+            placeholder={input.placeholder}
+          ></textarea>
+        ) : (
+          <input
+            className="rounded-md bg-transparent placeholder-gray-500 w-1/2"
+            name={input.name}
+            placeholder={input.placeholder}
+          />
+        )}
+      </div>
     );
   });
 };
 export default function ContactPage() {
   return (
-    <div className="mt-8 flex py-6 flex-col space-y-6 sm:w-1/2 mx-auto rounded-md drop-shadow-lg border border-eggplant dark:border-midnightGreen">
+    <div className="mt-8 py-6 space-y-6 sm:w-1/2 mx-auto rounded-md sm:shadow-lg border border-eggplant dark:border-midnightGreen ">
       <ContactForm />
-      <div className="flex justify-center">
+      <div className="flex justify-center h-10 py-2 hover:outline-double rounded-md mx-auto w-20 bg-brunswickGreen dark:bg-cherryBlossomPink text-brunswickGreen dark:text-cherryBlossomPink hover:bg-brunswickGreen50 dark:hover:bg-cherryBlossomPink50">
         <PaperPlaneTilt
           cursor={"pointer"}
-          size={32}
-          className=" rounded bg-eggplant50 text-brunswickGreen dark:text-cherryBlossomPink hover:bg-puce dark:hover:bg-eggplant100"
+          size={24}
+          className="text-cherryBlossomPink dark:text-brunswickGreen"
           type="submit"
         >
           Send
